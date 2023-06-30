d09a11647bda8f0dc82d2ab827d481a9131805d1
jx06T <50313tjx06@gmail.com>
2023-06-30 18:55:20 +0800

    new file:   .gitignore 	modified:   background.js 	modified:   manifest.json 	modified:   popup.html

--- background.js
+++ background.js
@@ -1,23 +1,45 @@
-let intervalId;
+let contextID = 0;
+console.log("jx00");
 
-chrome.runtime.onInstalled.addListener(() => {
-  startSimulatingKeys();
+chrome.input.ime.onActivate.addListener(() => {
+  console.log("onActivate");
 });
 
-function startSimulatingKeys() {
-  intervalId = setInterval(simulateKeys, 500);
-}
+chrome.input.ime.onDeactivated.addListener(() => {
+  console.log("onDeactivated");
+});
 
-function stopSimulatingKeys() {
-  clearInterval(intervalId);
-}
+chrome.input.ime.onFocus.addListener((context) => {
+  console.log("onFocus");
+  contextID = context.contextID;
+});
 
-function simulateKeys() {
-  // 模擬按下上鍵
+chrome.input.ime.onBlur.addListener(() => {
+  console.log("onBlur");
+  contextID = 0;
+})
+
+chrome.input.ime.onKeyEvent.addListener((engineID, keyData) =>{
+  const handled = false;
+
+  if (keyData.type == "keyup") return handled;
+  
+  console.log("alt=" + keyData.altKey + ",ctrl=" + keyData.ctrlKey + ",shift=" + keyData.shiftKey);
+  console.log("code=" + keyData.code + ",key=[" + keyData.key + "]");
+
+  return handled;
+});
+
+chrome.action.onClicked.addListener((tab) => {
+  const keyData = {
+    ctrlKey: true,
+    code: "KeyV",
+    key: "V",
+    type: "keydown"
+  };
+
   chrome.input.ime.sendKeyEvents({
-    contextID: 0,
-    keyData: [{ code: "ArrowUp", type: "keydown" }]
-  }, () => {});
-
- 
-}
+    contextID : contextID,
+    keyData :[keyData]
+  });
+});
